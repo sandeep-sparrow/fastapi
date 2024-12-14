53727eedea0d478071aadd44468a599294313fb6
Sandeep prajapati <61901052+sandeep-sparrow@users.noreply.github.com>
2024-12-14 19:15:15 +0530

    added get_user and change_password endpoints

--- main.py
+++ main.py
@@ -22,7 +22,6 @@ app.add_middleware(
 async def check_health():
     return "Up and Running!"
 app.include_router(todos.router, prefix='/todo', tags=['Todo'])
-
 app.include_router(users.router, prefix='/user', tags=['User'])
 app.include_router(admin.router, prefix='/admin/todo', tags=['Administration'])
 app.include_router(auth.router, prefix='/auth', tags=['Authentication'])
